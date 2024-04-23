dc077a5cadfde8c0e0c3818677d70c605c940c0c
ALOK PATEL <64703374+alokknight@users.noreply.github.com>
2024-04-23 22:19:27 +0530

    formated js code

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,4 @@
-/*
-================================================================
-* Template:  	 Simone - Personal Portfolio Template
-* Written by: 	 Harnish Design - (http://www.harnishdesign.net)
-* Description:   Main Custom Script File
-================================================================
-*/
+
 
 (function ($) {
 	"use strict";
@@ -21,8 +15,8 @@ $(window).on('load', function () {
 $(window).on('scroll',function() {
 	var stickytop = $('#header.sticky-top .bg-transparent');
 	var stickytopslide = $('#header.sticky-top-slide');
-	
-	if ($(this).scrollTop() > 1){  
+
+	if ($(this).scrollTop() > 1){
 		stickytop.addClass("sticky-on-top");
 		stickytop.find(".logo img").attr('src',stickytop.find('.logo img').data('sticky-logo'));
 	}
@@ -30,8 +24,8 @@ $(window).on('scroll',function() {
 		stickytop.removeClass("sticky-on-top");
 		stickytop.find(".logo img").attr('src',stickytop.find('.logo img').data('default-logo'));
 	}
-	
-	if ($(this).scrollTop() > 180){  
+
+	if ($(this).scrollTop() > 180){
 		stickytopslide.find(".primary-menu").addClass("sticky-on");
 		stickytopslide.find(".logo img").attr('src',stickytopslide.find('.logo img').data('sticky-logo'));
 	}
@@ -134,7 +128,7 @@ $(this).magnificPopup({
 });
 });
 
-// Ajax On Modal 
+// Ajax On Modal
 $('.popup-ajax-gallery').each(function() {
 $(this).magnificPopup({
 	delegate: '.popup-ajax:visible',
@@ -181,7 +175,7 @@ $(this).magnificPopup({
 					576:{items: a.data('items-sm'),},
 					768:{items: a.data('items-md'),},
 					992:{items: a.data('items-lg'),}
-				}	
+				}
                 });
             });
          }
@@ -215,9 +209,9 @@ $(".portfolio-filter").each(function() {
 		});
 	$(".portfolio-menu").find("a").on("click", function() {
         var filterValue = $(this).attr("data-filter");
-        return $(".portfolio-menu").find("a").removeClass("active"), $(this).addClass("active"), 
+        return $(".portfolio-menu").find("a").removeClass("active"), $(this).addClass("active"),
 		$grid.isotope({
-          filter: filterValue
+        filter: filterValue
         }), !1
     });
 	});
@@ -319,7 +313,7 @@ form.on('submit', function (e) {
 						}
 					},
 					success: function (data) {
-						submit.before(data.Message).fadeIn("slow"); // fade in response data 
+						submit.before(data.Message).fadeIn("slow"); // fade in response data
 						submit.html(submit.data('original-text'));// reset submit button text
 						submit.removeAttr("disabled", "disabled");
 						if (data.response == 'success') {
@@ -352,7 +346,7 @@ form.on('submit', function (e) {
 				}
 			},
 			success: function (data) {
-				submit.before(data.Message).fadeIn("slow"); // fade in response data 
+				submit.before(data.Message).fadeIn("slow"); // fade in response data
 				submit.html(submit.data('original-text'));// reset submit button text
 				submit.removeAttr("disabled", "disabled");
 				if (data.response == 'success') {
